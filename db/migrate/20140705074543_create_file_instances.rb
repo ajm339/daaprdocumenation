3c69ae8d40fbd817f47d8e3c4adffdc1417c872a
Prelang Builder <builder@prelang.com>
2014-07-05 07:45:43 +0000

    Generated model FileInstance with migration

---
+++ db/migrate/20140705074543_create_file_instances.rb
@@ -0,0 +1,13 @@
+class CreateFileInstances < ActiveRecord::Migration
+  def change
+    create_table :file_instances do |t|
+      t.integer :file_type
+      t.string :name
+      t.text :description
+      t.integer :id
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/file_instances.rb
@@ -0,0 +1,11 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :file_instance do
+    file_type 1
+    name "MyString"
+    description "MyText"
+    id 1
+    user_id 1
+  end
+end
