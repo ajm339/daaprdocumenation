8e81f4149bbd31a840e240b70441d1ca0f1ad22a
Prelang Builder <builder@prelang.com>
2014-07-05 07:45:50 +0000

    Generated model FileUpdate with migration

---
+++ db/migrate/20140705074550_create_file_updates.rb
@@ -0,0 +1,11 @@
+class CreateFileUpdates < ActiveRecord::Migration
+  def change
+    create_table :file_updates do |t|
+      t.integer :id
+      t.integer :documentation_id
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
