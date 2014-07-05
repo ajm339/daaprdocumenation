5a7463ab7fe36819c64bb02691216c4056b0f26a
Prelang Builder <builder@prelang.com>
2014-07-05 07:45:47 +0000

    Generated model FileMethod with migration

---
+++ db/migrate/20140705074546_create_file_methods.rb
@@ -0,0 +1,15 @@
+class CreateFileMethods < ActiveRecord::Migration
+  def change
+    create_table :file_methods do |t|
+      t.integer :id
+      t.integer :created_by
+      t.string :name
+      t.text :description
+      t.text :input
+      t.text :output
+      t.references :file_instance, index: true
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/file_methods.rb
@@ -0,0 +1,13 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :file_method do
+    id 1
+    created_by 1
+    name "MyString"
+    description "MyText"
+    input "MyText"
+    output "MyText"
+    file_instance nil
+  end
+end
